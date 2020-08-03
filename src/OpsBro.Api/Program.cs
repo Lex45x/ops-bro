837c72985b43241e047313c72e54cb463f7fd94c
MisterHoker <redka2av@gmail.com>
2020-08-03 22:25:45 +0200

    Added NLog. todo: Add ability to configure log level via environment variable.

--- src/OpsBro.Api/Program.cs
+++ src/OpsBro.Api/Program.cs
@@ -1,6 +1,12 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+using NLog.Web;
 using OpsBro.Domain.Settings;
+using System;
 using System.Threading.Tasks;
 
 namespace OpsBro.Api
@@ -9,8 +15,27 @@ namespace OpsBro.Api
     {
         public static async Task Main(string[] args)
         {
-            var settingsFactory = new SettingsFactory();
-            Settings = await settingsFactory.CreateSettings();
+            var config = new ConfigurationBuilder()
+               .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+               .Build();
+
+            LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
+            
+            var logger = LogManager.GetCurrentClassLogger();
+
+            logger.Info("Welcome to OpsBro! -- this message means that logging is configured and working correctly");
+
+            try
+            {
+                var settingsFactory = new SettingsFactory();
+                Settings = await settingsFactory.CreateSettings();
+            }
+            catch (Exception exception)
+            {
+                logger.Fatal(exception, "Settings deserialization is failed!");
+                throw;
+            }
 
             BuildWebHost(args).Run();
         }
@@ -20,6 +45,12 @@ namespace OpsBro.Api
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .ConfigureLogging((logging) =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog()
                 .Build();
     }
 }
